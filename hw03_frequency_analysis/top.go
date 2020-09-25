c9c461eb252cffb3b6644923c9da290440127c17
Ivan <ivfisunov@gmail.com>
2020-09-26 00:39:56 +0300

    HW3 is done

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,39 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
 
-func Top10(_ string) []string {
-	// Place your code here
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type kv struct {
+	key string
+	value int
+}
+
+func Top10(text string) []string {
+	if text == "" {
+		return []string{}
+	}
+	splitedText := strings.Fields(text)
+
+	frequencyMap := make(map[string]int)
+	for _, word := range splitedText {
+		frequencyMap[word]++
+	}
+
+	var freqStore []kv
+	for k, v := range frequencyMap {
+		freqStore = append(freqStore, kv{k, v})
+	}
+	sort.Slice(freqStore, func(i, j int) bool {
+		return freqStore[i].value > freqStore[j].value
+	})
+	freqStore = freqStore[:10]
+
+	var result []string
+	for _, v := range freqStore {
+		result = append(result, v.key)
+	}
+
+	return result
 }
