9988f6e94793a01f3bb3e69086c444b972ec0f55
Ivan <ivfisunov@gmail.com>
2020-09-29 10:55:59 +0300

    hw 4 completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,22 +1,72 @@
 package hw04_lru_cache //nolint:golint,stylecheck
 
+import (
+	"sync"
+)
+
 type Key string
 
 type Cache interface {
-	// Place your code here
+	Set(key Key, value interface{}) bool // Добавить значение в кэш по ключу
+	Get(key Key) (interface{}, bool)     // Получить значение из кэша по ключу
+	Clear()                              // Очистить кэш
 }
 
 type lruCache struct {
-	// Place your code here:
-	// - capacity
-	// - queue
-	// - items
+	cap   int
+	queue List
+	cache map[Key]*listItem
+	sync.Mutex
 }
 
 type cacheItem struct {
-	// Place your code here
+	key   Key
+	value interface{}
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	c.Mutex.Lock()
+	defer c.Mutex.Unlock()
+
+	if _, ok := c.cache[key]; !ok {
+		return nil, false
+	} else {
+		c.queue.MoveToFront(c.cache[key])
+		c.cache[key] = c.queue.Front()
+		return c.cache[key].Value.(*cacheItem).value, true
+	}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	c.Mutex.Lock()
+	defer c.Mutex.Unlock()
+
+	if _, ok := c.cache[key]; !ok {
+		if c.queue.Len() >= c.cap {
+			c.Clear()
+		}
+		newCacheItem := &cacheItem{value: value, key: key}
+		c.queue.PushFront(newCacheItem)
+		c.cache[key] = c.queue.Front()
+		return false
+	} else {
+		c.cache[key].Value.(*cacheItem).value = value
+		c.queue.MoveToFront(c.cache[key])
+		c.cache[key] = c.queue.Front()
+		return true
+	}
+}
+
+func (c *lruCache) Clear() {
+	lastItem := c.queue.Back().Value.(*cacheItem)
+	c.queue.Remove(c.queue.Back())
+	delete(c.cache, lastItem.key)
 }
 
 func NewCache(capacity int) Cache {
-	return &lruCache{}
+	return &lruCache{
+		cap:   capacity,
+		queue: NewList(),
+		cache: make(map[Key]*listItem),
+	}
 }
--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -1,6 +1,7 @@
 package hw04_lru_cache //nolint:golint,stylecheck
 
 import (
+	// "fmt"
 	"math/rand"
 	"strconv"
 	"sync"
@@ -49,8 +50,50 @@ func TestCache(t *testing.T) {
 		require.Nil(t, val)
 	})
 
-	t.Run("purge logic", func(t *testing.T) {
+	t.Run("pop first item", func(t *testing.T) {
 		// Write me
+		c := NewCache(3)
+		c.Set("aaa", 100)
+		c.Set("bbb", 200)
+		c.Set("ccc", 300)
+		c.Set("ccc", 333)
+		c.Set("ddd", 400)
+
+		val, ok := c.Get("aaa")
+		require.False(t, ok)
+		require.Nil(t, val)
+	})
+
+	t.Run("pop rarely used etem", func(t *testing.T) {
+		c := NewCache(5)
+		c.Set("aaa", 100)
+		c.Set("bbb", 200)
+		c.Set("ccc", 300)
+		c.Set("ddd", 400)
+		c.Set("eee", 555)
+		val, ok := c.Get("eee")
+		require.True(t, ok)
+		require.Equal(t, 555, val);
+		c.Get("bbb")
+		c.Get("bbb")
+		c.Set("bbb", 222)
+		c.Get("bbb")
+		c.Set("ccc", 333)
+		c.Get("ccc")
+		c.Get("ccc")
+		c.Get("ccc")
+		c.Get("ddd")
+		c.Set("ddd", 404)
+		c.Set("ddd", 444)
+		c.Get("ddd")
+		c.Set("NEW", 999)
+		val, ok = c.Get("bbb")
+		require.True(t, ok)
+		require.Equal(t, 222, val);
+
+		val, ok = c.Get("aaa")
+		require.False(t, ok)
+		require.Nil(t, val)
 	})
 }
 
